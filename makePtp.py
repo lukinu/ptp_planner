4e43c3645bd1e08fd068d21865611004b15ad60e
Iurii Lukin <y.v.lukin@gmail.com>
2025-08-15 23:37:40 +0500

    switched to json implementation

--- makePtp.py
+++ makePtp.py
@@ -1,21 +1,25 @@
-import xml.etree.ElementTree as ET
+import json
+import csv
+import zipfile
 
-# Load your XML template
-tree = ET.parse('./ptp_project_tmpl.kml')
-root = tree.getroot()
+BRAND = 'http://infinet.ru/kml'
+HEAD = '<?xml version="1.0" encoding="UTF-8"?><kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:projectdata="' + BRAND + '"><Document id="1t-UlJIsJjQEInqy4VwWc-mk-s5xufjVo"><name>New Project</name><open>1</open><ExtendedData><projectdata:json>'
+TAIL = '</projectdata:json></ExtendedData></Document></kml>'
+sitesArray = []
+linksArray = []
 
-for child in root:
-    print(child.tag, child.attrib)
-    
-# Find an element and update its text
-element_to_update = root.find('Site C - Site D')
-if element_to_update is not None:
-#    element_to_update.text = 'New Value'
-    print(element_to_update.tag)
-    
-# Add a new element
-#new_element = ET.SubElement(root, 'new_data')
-#new_element.text = 'Some new information'
+with open('links.csv', newline='') as csvfile:
+    links_lst = csv.reader(csvfile, delimiter=',')
+    link = 
+    linksArray.append(link)
+    # for row in links_lst:
+        # print(', '.join(row))
 
-# Save the modified XML
-#tree.write('updated_output.kml', encoding='utf-8', xml_declaration=True)
+jstring = json.loads('{"appVersion":"f4eb57c","appVersionFull":"f4eb57c","linksArray":[{"terrainType":"AVERAGE","climateType":"NORMAL","frequencies":{"start":4850,"end":6050},"band":5450,"transmissionType":"SINGLE_CARRIER","bandwidth":10,"goal":{"type":"DISTANCE","value":30000},"txPowerLimit":"Infinity","temperature":293.15,"totalAirPressure":1013.2499968000001,"humidity":60,"startSite":{"id":782582,"name":"ÇİM-NAK","location":{"latitude":40.7797151668,"longitude":29.5982600517},"antennaHeight":17,"deviceProductKey":"Quanta 5#Q5-23","antennaPartNumber":null,"rfCablePartNumber":null,"relocationLocked":false,"interference":"-Infinity","temperature":293.1499938964844,"powerLimit":null},"endSite":{"id":782581,"name":"kamera diregi","location":{"latitude":40.7905567792,"longitude":29.5967539525},"antennaHeight":15,"deviceProductKey":"Quanta 5#Q5-23","antennaPartNumber":null,"rfCablePartNumber":null,"relocationLocked":false,"interference":"-Infinity","temperature":293.1499938964844,"powerLimit":null}}],"sitesArray":[{"id":782582,"name":"ÇİM-NAK","location":{"latitude":40.7797151668,"longitude":29.5982600517},"antennaHeight":17,"deviceProductKey":"Quanta 5#Q5-23","antennaPartNumber":null,"rfCablePartNumber":null,"relocationLocked":false,"interference":"-Infinity","temperature":293.1499938964844,"powerLimit":null},{"id":782581,"name":"kamera diregi","location":{"latitude":40.7905567792,"longitude":29.5967539525},"antennaHeight":15,"deviceProductKey":"Quanta 5#Q5-23","antennaPartNumber":null,"rfCablePartNumber":null,"relocationLocked":false,"interference":"-Infinity","temperature":293.1499938964844,"powerLimit":null}],"obstaclesArray":[],"project":{"id":78620,"name":"Burkina-Faso","type":"PTP","regulation":"WORLDWIDE","unitSystem":"METRIC","settings":{"ptmp":{"visible":true}},"updatedDatetime":"2025-05-02T09:22:54.718996Z","eirpInfo":{"otherLimit":50,"freqLimits":[]},"createNew":0}}')
+print(json.dumps(jstring, indent=4))
+
+with open("file.kml", "w") as f:
+    f.write(HEAD + TAIL)
+
+# f = open("file.kml","r")
+# print(f.read())
